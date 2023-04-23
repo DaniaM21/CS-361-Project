97ec5bbd46215a9d17c4577498365db033255af8
DaniaM21 <danianunez19@gmail.com>
2023-04-22 17:28:07 -0700

    added user interaction to project

---
+++ UI.py
@@ -0,0 +1,21 @@
+from ToDoList_Functions import *
+
+print('*****')
+print('Welcome to To-Doey!')
+print('To-Doey lets you manage your to-do lists.')
+print('Use the following menu to manage your lists.')
+print('*****')
+
+while True:
+    startMenu()
+    option = int(input('Enter the number next to the option in the Start Menu to perform that function: '))
+    if option == 1:
+        createList()
+    elif option == 2:
+        searchLists()
+    elif option == 3:
+        viewLists()
+    elif option == 4:
+        exit()
+    else:
+        print('Invalid option. Please try again.')
